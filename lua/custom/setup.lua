c99345207bd6ea3dc073330cac8b722cede22ea2
Fraser Fewster <fraser.fewster@imaginelearning.com>
2026-05-12 16:09:49 +0100

    Fix helm lsp

--- lua/custom/setup.lua
+++ lua/custom/setup.lua
@@ -1,3 +1,11 @@
 if vim.fn.executable 'tree-sitter' == 0 then
   vim.schedule(function() vim.notify('tree-sitter-cli not found! Run: npm install -g tree-sitter-cli', vim.log.levels.WARN) end)
 end
+
+vim.filetype.add {
+  pattern = {
+    ['.*/templates/.*%.yaml'] = 'helm',
+    ['.*/templates/.*%.tpl'] = 'helm',
+    ['helmfile.*%.yaml'] = 'helm',
+  },
+}
